9c08fe66ee6ae86ae5f257c8f44b88ceabfcb05b
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-20 19:50:34 -0600

    added map functionality and password field for new user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def display_map(park)
+    "https://www.google.com/maps/embed/v1/place?key=#{ENV['MAPS_API_KEY']}&q=<%= park.street_address %>, Chicago, IL" 
+  end
 end
